cc51a092cf92ebd6513e3b45f8fad061787e053b
Wendel Nascimento <wendel.nascimento2@outlook.com>
2016-07-14 17:36:38 -0300

    Inclusão de botão continuar(Ele só não funciona bem)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
 import AddClassScroll from './AddClassScroll';
 import scss from '../scss/main.scss';
 import fontAwesome from '../../node_modules/font-awesome/scss/font-awesome.scss';
+
+let intro = new AddClassScroll("#intro-button")
